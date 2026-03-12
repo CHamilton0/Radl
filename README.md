2068d161d1bce57f880aa2f3aaf1dc58dd5cc803
Christopher Hamilton <chrish001@live.com.au>
2026-03-12 22:14:29 +1030

    feat: start adding summarisation of articles

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Radl
+
 An Adelaide aggregator for news and current events
+
+```bash
+sudo mkdir /opt/libtorch
+sudo curl https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-2.4.0%2Bcpu.zip -o /tmp/libtorch.zip
+sudo unzip /tmp/libtorch.zip -d /tmp/libtorch-tmp
+sudo mv /tmp/libtorch-tmp/libtorch /opt/libtorch
+sudo rm -rf /tmp/libtorch-tmp /tmp/libtorch.zip
+
+export LIBTORCH=/opt/libtorch
+export LD_LIBRARY_PATH=${LIBTORCH}/lib:$LD_LIBRARY_PATH
+export RUSTFLAGS="-C linker=gcc"
+```
--- src/glamadelaide.rs
+++ src/glamadelaide.rs
@@ -1,9 +1,13 @@
 use reqwest::Client;
 use select::document::Document;
-use select::predicate::Class;
+use select::predicate::{Class, Name};
+use rust_bert::pipelines::summarization::SummarizationModel;
 
 #[tokio::main]
 pub async fn get_this_week(client: Client) -> Result<(), Box<dyn std::error::Error>> {
+    // Create model once before fetching pages
+    let model = tokio::task::block_in_place(|| SummarizationModel::new(Default::default()))?;
+
     // Loop through all the GlamAdelaide pages
     let mut page_number = 1;
     loop {
@@ -23,18 +27,34 @@ pub async fn get_this_week(client: Client) -> Result<(), Box<dyn std::error::Err
         let body = resp.text().await?;
         let document = Document::from(body.as_str());
 
-        // Get each post and print the text
-        for node in document.find(Class("infinite-post")) {
-            let trimmed: String = node.text().trim().split_once("\n").iter().collect::<Vec<_>>()[0]
+        // Get each post this week
+        for post_node in document.find(Class("infinite-post")) {
+            let trimmed: String = post_node
+                .text()
+                .trim()
+                .split_once("\n")
+                .iter()
+                .collect::<Vec<_>>()[0]
                 .1
                 .chars()
                 .filter(|c| *c != '\t')
                 .collect();
             let mut lines = trimmed.lines().filter(|l| !l.is_empty());
+            
+            // Get the title and body from the article
             let title = lines.next().unwrap();
             let body = lines.next().unwrap();
             println!("{title}");
             println!("{body}");
+
+            let output = tokio::task::block_in_place(|| model.summarize(&[body]));
+            println!("{:?}", output);
+
+            // Get the first link element in the post node and get the link from it
+            if let Some(href) = post_node.find(Name("a")).next().unwrap().attr("href") {
+                println!("{}", href);
+            }
+
             println!();
         }
 
